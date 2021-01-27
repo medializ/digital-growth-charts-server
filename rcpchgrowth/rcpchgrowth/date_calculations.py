e624c6f873dad33f5cac07ded52c0e5addd3e6c4
Simon Chapman <eatyourpeasapps@gmail.com>
2021-01-27 15:46:44 +0000

    age-correction

--- rcpchgrowth/rcpchgrowth/date_calculations.py
+++ rcpchgrowth/rcpchgrowth/date_calculations.py
@@ -5,23 +5,12 @@ import math
 from .constants import TERM_PREGNANCY_LENGTH_DAYS, TERM_LOWER_THRESHOLD_LENGTH_DAYS, EXTREME_PREMATURITY_THRESHOLD_LENGTH_DAYS
 
 """
-5 functions to calculate age related parameters
- - decimal_age: returns a decimal age from 2 dates (birth_date and observation_date) and gestational age at delivery (gestation_weeks and gestation_days), based on 40 weeks as 0. Days/Weeks before 40 weeks are negative.
+4 functions to calculate age related parameters
  - chronological_decimal_age: returns a decimal age from 2 dates (takes birth_date and observation_date)
  - corrected_decimal_age: returns a corrected decimal age accounting for prematurity (takes birth_date: date, observation_date: date, gestation_weeks: int, gestation_days: int, pregnancy_length_day [optional])
  - chronological_calendar_age: returns a calendar age as a string (takes birth_date or estimated_date_delivery and observation_date)
  - estimated_date_delivery: returns estimated date of delivery in a known premature infant (takes birth_date, gestation_weeks, gestation_days, pregnancy_length_days[optional])
 """
-
-def decimal_age(birth_date: date, observation_date: date, gestation_weeks: int, gestation_days: int):
-    """
-    returns decimal age relative to forty weeks expressed as 0 y
-    THIS FUNCTION IS DEPRECATED
-    """
-    days_born_from_forty_weeks = ((gestation_weeks * 7) + gestation_days) - TERM_PREGNANCY_LENGTH_DAYS
-    days_of_life_in_years = chronological_decimal_age(birth_date, observation_date)
-    return  (days_born_from_forty_weeks / 365.25) + days_of_life_in_years
-
 
 def chronological_decimal_age(birth_date: date, observation_date: date) -> float:
 
@@ -39,43 +28,27 @@ def chronological_decimal_age(birth_date: date, observation_date: date) -> float
 
 def corrected_decimal_age(birth_date: date, observation_date: date, gestation_weeks: int, gestation_days: int)->float: 
     """
-    Corrects for gestational age. 
-    Corrects for 1 year corrected age, if gestation at birth >= 32 weeks and < 37 weeks
-    Corrects for 2 years corrected age, if gestation at birth <32 weeks
-    Otherwise returns decimal age without correction
-    Any baby 37-42 weeks returns decimal age of 0.0
-    Depends on chronological_decimal_age
+    Corrects for gestational age. Does not account for term.
     :param birth_date: date of birth
     :param observation_date: date observation made
-    :param gestation_weeks: weeks of gestation up to 40
+    :param gestation_weeks: weeks of gestation up to 42
     :param gestation_days: days in excess of weeks
+    :chronological_decimal_age is a dependency
     """
     
     correction_days = 0
-    pregnancy_length_days = TERM_PREGNANCY_LENGTH_DAYS
 
-    if gestation_weeks > 0 and gestation_weeks < 37:
-        pregnancy_length_days = (gestation_weeks * 7) + gestation_days
-
-    try:
-        uncorrected_age = chronological_decimal_age(birth_date, observation_date)
-    except ValueError as err:
-        return err
-
+    if gestation_weeks == 0:
+        gestation_weeks=40
+    
+    pregnancy_length_days = (gestation_weeks * 7) + gestation_days
 
     ## age correction
     correction_days = TERM_PREGNANCY_LENGTH_DAYS - pregnancy_length_days
     edd = birth_date + timedelta(days=correction_days)
     corrected_age =  chronological_decimal_age(edd, observation_date)
 
-    if pregnancy_length_days < EXTREME_PREMATURITY_THRESHOLD_LENGTH_DAYS and corrected_age <= 2:
-        #correct age for 2 years
-        return corrected_age
-    elif (pregnancy_length_days < TERM_LOWER_THRESHOLD_LENGTH_DAYS) and corrected_age <= 1:
-        #correct age for 1 year
-        return corrected_age
-    else:
-        return uncorrected_age
+    return corrected_age
     
 
 
